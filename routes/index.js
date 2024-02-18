533d6c73b56446f38874dfde35eb30057d6afd6b
GabyAM <gdgabyam@gmail.com>
2024-02-18 16:02:20 -0300

    implements separate signup page to select tier
    now, instead of directly showing the signup form for the member tier, the user first has to select wich tier he decides to sign in to. For the moment, only the member tier is implemented, but i intend to add the user and admin tiers.

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,14 @@ router.get('/', messageController.index);
 router.post('/', messageController.indexMessagePost);
 
 router.get('/signup', (req, res, next) => {
+    res.render('sign_up_select');
+});
+
+router.get('/signup/member', (req, res, next) => {
     res.render('sign_up_form');
 });
 
-router.post('/signup', userController.createUserPost);
+router.post('/signup/member', userController.createUserPost);
 
 router.get('/login', userController.loginUserGet);
 router.post('/login', userController.loginUserPost);
