1a160fb368805d715a7ea881f07bf1f58ff525ff
GabyAM <gdgabyam@gmail.com>
2024-02-20 12:20:07 -0300

    implements date on messages
    now messages display a date, formatted to show the date and hour.

--- models/message.js
+++ models/message.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const messageSchema = new Schema(
     {
         user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
         title: String,
-        text: String,
-        created_at: Date
+        text: String
     },
     { timestamps: true }
 );
+
+messageSchema.virtual('createdAt_formatted').get(function () {
+    return DateTime.fromJSDate(this.createdAt).toLocaleString(
+        DateTime.DATETIME_FULL
+    );
+});
 
 const Message = mongoose.model('Message', messageSchema);
 module.exports = Message;
